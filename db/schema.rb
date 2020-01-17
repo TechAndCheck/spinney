65a85db43132d5db64b94079dfdcb3cc528f0739
Christopher Guess <cguess@gmail.com>
2020-01-16 21:33:12 -0500

    Move from Sqlite to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_16_233238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "api_keys", force: :cascade do |t|
     t.string "key", null: false
     t.string "name"
